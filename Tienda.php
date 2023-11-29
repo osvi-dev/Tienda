9715994854046da8dfeb8fa8da06e2a44da90195
Jose Bautista <107967041+osvi-dev@users.noreply.github.com>
2023-11-29 10:02:34 -0600

    Falta que se actualice en recarga

--- Tienda.php
+++ Tienda.php
@@ -29,122 +29,165 @@ $querySalida = "SELECT * FROM Salida";
 $resultSalida = $conn->query($querySalida);
 
 // Función para agregar una nueva compra
-function agregarCompra($conn, $producto, $cantidad, $precio) {
-    // Agrega la lógica para insertar la compra en la base de datos
-    // ...
-    return true; // Devuelve true por ahora, ajusta según sea necesario
+
+// Función para agregar una nueva compra
+function agregarCompra($conn, $proveedor, $producto, $cantidad, $precio)
+{
+    // Actualizar la tabla Almacen
+    $updateAlmacen = "UPDATE Almacen SET Stock = Stock + $cantidad WHERE ID = $producto";
+    if ($conn->query($updateAlmacen) !== TRUE) {
+        return false; // Si hay un error al actualizar, devolver false
+    }
+
+    // Insertar la compra en la tabla Compra
+    $insertCompra = "INSERT INTO Compra (IDProvedor, IDProducto, Cant, Precio) VALUES ($proveedor, $producto, $cantidad, $precio)";
+    if ($conn->query($insertCompra) !== TRUE) {
+        return false; // Si hay un error al insertar, devolver false
+    }
+
+    return true; // Todo se realizó con éxito
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Si se ha enviado el formulario de compra
+    $proveedor = $_POST["proveedor"];
     $producto = $_POST["producto"];
     $cantidad = $_POST["cantidad"];
     $precio = $_POST["precio"];
 
-    if (agregarCompra($conn, $producto, $cantidad, $precio)) {
+    if (agregarCompra($conn, $proveedor, $producto, $cantidad, $precio)) {
         echo '<script>alert("Compra agregada con éxito");</script>';
     } else {
         echo '<script>alert("Error al agregar la compra");</script>';
     }
 }
 
+
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda en Línea</title>
     <style>
-body {
+        body {
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
             background-color: #f4f4f4;
         }
+
         .navbar {
-            background-color: #0066cc; /* Color de fondo de la barra */
+            background-color: #0066cc;
+            /* Color de fondo de la barra */
             color: white;
             padding: 10px 20px;
             display: flex;
             align-items: center;
         }
+
         .navbar img {
-            width: 50px; /* Ajusta el tamaño del logo según necesidades */
+            width: 50px;
+            /* Ajusta el tamaño del logo según necesidades */
             height: auto;
             margin-right: 15px;
         }
+
         .navbar h1 {
             margin: 0;
             font-size: 24px;
         }
+
         header {
             padding: 20px;
             text-align: center;
-            background-color: #333; /* Mantener este color o cambiarlo si se desea */
+            background-color: #333;
+            /* Mantener este color o cambiarlo si se desea */
             color: white;
         }
+
         table {
             margin: 20px auto;
             border-collapse: collapse;
             width: 80%;
         }
-        th, td {
+
+        th,
+        td {
             padding: 8px;
             border: 1px solid #ddd;
             text-align: left;
         }
+
         thead {
             background-color: #007bff;
             color: white;
         }
+
         tbody tr:nth-child(even) {
             background-color: #f2f2f2;
         }
+
         table {
-    margin: 20px auto;
-    border-collapse: collapse;
-    width: 80%;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); /* Sombra para efecto elevado */
-}
+            margin: 20px auto;
+            border-collapse: collapse;
+            width: 80%;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            /* Sombra para efecto elevado */
+        }
 
-th, td {
-    padding: 12px;
-    border: 1px solid #ddd;
-    text-align: center; /* Centrado para hacerlo más legible */
-}
+        th,
+        td {
+            padding: 12px;
+            border: 1px solid #ddd;
+            text-align: center;
+            /* Centrado para hacerlo más legible */
+        }
 
-thead {
-    background-color: #007bff;
-    color: white;
-}
+        thead {
+            background-color: #007bff;
+            color: white;
+        }
 
-tbody tr:hover {
-    background-color: #ddd; /* Efecto hover para las filas */
-}
+        tbody tr:hover {
+            background-color: #ddd;
+            /* Efecto hover para las filas */
+        }
 
-tbody tr:nth-child(even) {
-    background-color: #f2f2f2;
-}
+        tbody tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
 
-h1 {
-    color: #333;
-    margin-top: 50px; /* Espaciado adicional antes de las secciones */
-}
-.table-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    width: 100%;
-}
+        h1 {
+            color: #333;
+            margin-top: 50px;
+            /* Espaciado adicional antes de las secciones */
+        }
 
-.table-container {
-    width: 45%; /* Ajusta esto según el ancho deseado para cada tabla */
-    margin: 0 2%; /* Espaciado entre las tablas */
-    /* Aplica aquí estilos adicionales si es necesario */
-}    </style>
+        .table-wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: flex-start;
+            width: 100%;
+        }
+
+        .table-container {
+            width: 45%;
+            /* Ajusta esto según el ancho deseado para cada tabla */
+            margin: 0 2%;
+            /* Espaciado entre las tablas */
+            /* Aplica aquí estilos adicionales si es necesario */
+        }
+        h2{
+            display: flex;
+            justify-content: center;
+        }
+    </style>
 </head>
+
 <body>
     <div class="navbar">
         <img src="cha.jpg" alt="Logo de la Tienda">
@@ -190,7 +233,7 @@ h1 {
             <table>
                 <thead>
                     <tr>
-                        <th>ID Compra</th>
+                        <th>ID Proveedor</th>
                         <th>ID Producto</th>
                         <th>Cantidad Comprada</th>
                         <th>Precio</th>
@@ -210,21 +253,6 @@ h1 {
                     ?>
                 </tbody>
             </table>
-
-            <!-- Agregar un formulario para agregar una nueva compra -->
-            <h2>Agregar Compra</h2>
-            <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" onsubmit="return validarCompra()">
-                <label for="producto">ID Producto:</label>
-                <input type="text" id="producto" name="producto" required>
-
-                <label for="cantidad">Cantidad:</label>
-                <input type="number" id="cantidad" name="cantidad" required>
-
-                <label for="precio">Precio:</label>
-                <input type="number" id="precio" name="precio" required>
-
-                <button type="submit">Agregar Compra</button>
-            </form>
         </div>
     </div>
 
@@ -284,4 +312,32 @@ h1 {
         </div>
     </div>
 
-    <script>
+    <!-- Agregar un formulario para agregar una nueva compra -->
+    <h2>Agregar Compra</h2>
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" onsubmit="return validarCompra()">
+        <label for="producto">ID Producto:</label>
+        <input type="text" id="producto" name="producto" required>
+        
+        <label for="proveedor">ID Proveedor:</label>
+        <input type="text" id="proveedor" name="proveedor" required>
+
+        <label for="cantidad">Cantidad:</label>
+        <input type="number" id="cantidad" name="cantidad" required>
+
+        <label for="precio">Precio:</label>
+        <input type="number" id="precio" name="precio" required>
+
+        <button type="submit" onclick="actualizarCompras()">Agregar Compra</button>
+        </form>
+
+</script>
+<script>
+    // Función para recargar la página después de agregar una compra
+    function actualizarCompras() {
+        setTimeout(function () {
+            location.reload(); // Recargar la página después de 1 segundo
+        }, 1000);
+    }
+</script>
+</body>
+</html>
